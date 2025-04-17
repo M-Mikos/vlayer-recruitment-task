bf11ea0bd50d2c1715bda92fbf3e5080efbc34c6
M-Mikos <mikos.marcin.m@gmail.com>
2025-04-17 23:57:12 +0200

    Add testimonials content

--- src/app/_components/page/section-testimonials/section-testimonials.tsx
+++ src/app/_components/page/section-testimonials/section-testimonials.tsx
@@ -10,7 +10,28 @@ import { Paragraph } from "@/common/components/ui/paragraph";
 function SectionTestimonials() {
   const testimonialsData = [
     {
-      uid: "",
+      uid: "1",
+      content:
+        "Estatery is the platform I go to on almost a daily basis for 2nd home and vacation condo shopping, or to just look at dream homes in new areas. Thanks for fun home shopping and comparative analyzing, Estatery!",
+      authorName: "Mira Culos",
+      authorRole: "Renter",
+      autorPhoto: "/testimonials/mira-culos.avif",
+    },
+    {
+      uid: "2",
+      content:
+        "I check Estatery almost every day — whether I'm seriously house hunting or just daydreaming about future getaways. It makes exploring new locations and comparing properties incredibly easy and enjoyable.",
+      authorName: "Mark Brown",
+      authorRole: "Renter",
+      autorPhoto: "/testimonials/mark-brown.avif",
+    },
+    {
+      uid: "3",
+      content:
+        "Estatery turns home shopping into a daily delight. Whether I'm planning for the future or just exploring what's out there, I always find something exciting and new.",
+      authorName: "Jake White",
+      authorRole: "Renter",
+      autorPhoto: "/testimonials/jake-white.avif",
     },
   ];
 
@@ -29,7 +50,11 @@ function SectionTestimonials() {
         <Carousel>
           <CarouselContent>
             {testimonialsData.map((item) => (
-              <CarouselItem key={item.uid}></CarouselItem>
+              <CarouselItem key={item.uid} className="flex justify-center py-8">
+                <Paragraph variant="large" className="max-w-3xl text-center">
+                  {item.content}
+                </Paragraph>
+              </CarouselItem>
             ))}
           </CarouselContent>
         </Carousel>
