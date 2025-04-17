6b76eca1a852f0993449188c53a3bbb04ef98111
M-Mikos <mikos.marcin.m@gmail.com>
2025-04-17 09:48:59 +0200

    Add header action buttons

--- src/app/_components/layout/header/actions.tsx
+++ src/app/_components/layout/header/actions.tsx
@@ -1,5 +1,16 @@
+import { Button } from "@/common/components/ui/button";
+
 function Actions() {
-  return <div></div>;
+  return (
+    <div className="flex w-full gap-6 flex-col lg:flex-row lg:gap-4">
+      <Button className="w-full lg:w-32" variant="secondary">
+        Login
+      </Button>
+      <Button className="w-full lg:w-32" variant="primary">
+        Sign up
+      </Button>
+    </div>
+  );
 }
 
 export default Actions;
