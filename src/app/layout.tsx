1d51dce400b9daa7309fde3d5c74876320ea4ac6
M-Mikos <mikos.marcin.m@gmail.com>
2025-04-17 10:51:19 +0200

    Add main font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,20 @@
 import "./globals.css";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import type { Metadata } from "next";
 import Header from "./_components/layout/header/header";
 import Footer from "./_components/layout/footer/footer";
+import clsx from "clsx";
 
 export const metadata: Metadata = {
   title: "",
   description: "",
 };
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-plusjakartasans",
+});
 
 export default function RootLayout({
   children,
@@ -15,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={clsx("overflow-x-hidden", plusJakartaSans.className)}>
         <Header />
         {children}
         <Footer />
