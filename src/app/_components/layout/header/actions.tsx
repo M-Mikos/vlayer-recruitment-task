932277264ee867f002c0a31a759ec39975d2ef5a
M-Mikos <mikos.marcin.m@gmail.com>
2025-04-17 10:52:13 +0200

    Add header action buttons responsive styles

--- src/app/_components/layout/header/actions.tsx
+++ src/app/_components/layout/header/actions.tsx
@@ -2,11 +2,11 @@ import { Button } from "@/common/components/ui/button";
 
 function Actions() {
   return (
-    <div className="flex w-full gap-6 flex-col lg:flex-row lg:gap-4">
-      <Button className="w-full lg:w-32" variant="secondary">
+    <div className="flex w-full gap-6 flex-col sm:flex-row sm:gap-4">
+      <Button className="w-full sm:basis-1/2 lg:w-32" variant="secondary">
         Login
       </Button>
-      <Button className="w-full lg:w-32" variant="primary">
+      <Button className="w-full sm:basis-1/2 lg:w-32" variant="primary">
         Sign up
       </Button>
     </div>
