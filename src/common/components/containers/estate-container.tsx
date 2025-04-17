be34745099d6ea549ba5a15c00057657c7dc5ecf
M-Mikos <mikos.marcin.m@gmail.com>
2025-04-17 16:39:22 +0200

    Fix styles and classes at estate container

--- src/common/components/containers/estate-container.tsx
+++ src/common/components/containers/estate-container.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import { Button } from "../ui/button";
 import Heart from "../icons/heart";
 import Stars from "../icons/stars";
+import { Heading } from "../ui/heading";
+import clsx from "clsx";
 
 interface EstateContainerProps {
   title: string;
@@ -12,6 +14,7 @@ interface EstateContainerProps {
   category: string;
   categoryVisible: boolean;
   addToFavVisible: boolean;
+  className?: string;
 }
 
 function EstateContainer({
@@ -23,11 +26,17 @@ function EstateContainer({
   category,
   categoryVisible,
   addToFavVisible,
+  className,
 }: EstateContainerProps) {
   return (
-    <article className="w-full max-w-[22rem] rounded-lg overflow-hidden border border-base-200">
-      <div className="w-full h-48 mb-32">
-        <Image src={imageSrc} alt={imageAlt} fill />
+    <article
+      className={clsx(
+        "w-full max-w-[22rem] transition-all rounded-lg overflow-hidden border border-base-200 hover:shadow-[0_4px_40px_rgba(230,230,237,1)]",
+        className
+      )}
+    >
+      <div className="w-full h-48">
+        <Image src={imageSrc} alt={imageAlt} width={352} height={200} className="w-full h-full object-cover" />
       </div>
       {categoryVisible && (
         <div className="flex gap-1 items-center justify-center w-24 h-8 text-xs rounded-lg rounded-bl-none bg-main-primary text-main-white uppercase -mx-4 -ml-2">
@@ -35,21 +44,25 @@ function EstateContainer({
           {category}
         </div>
       )}
-      <div className="flex gap-6 mb-2">
-        <div>
-          <div className="flex items-center mb-1">
-            <span className="font-extrabold text-2xl text-secondary-500 tracking-tight">${price}</span>
-            <span className="font-medium text-main-black/50">/month</span>
+      <div className="p-5 bg-white">
+        <div className="flex gap-6 mb-2">
+          <div>
+            <div className="flex items-center gap-1 mb-1">
+              <span className="font-extrabold text-2xl text-secondary-500 tracking-tight">
+                ${price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              </span>
+              <span className="font-medium text-main-black/50">/month</span>
+            </div>
+            <Heading variant="h4">{title}</Heading>
           </div>
-          <h4 className="font-bold text-2xl tracking-tight">{title}</h4>
+          {addToFavVisible && (
+            <Button variant="icon">
+              <Heart />
+            </Button>
+          )}
         </div>
-        {addToFavVisible && (
-          <Button variant="icon">
-            <Heart />
-          </Button>
-        )}
+        <span className="font-medium text-main-black/50">{address}</span>
       </div>
-      <address className="font-medium text-main-black/50">{address}</address>
     </article>
   );
 }
