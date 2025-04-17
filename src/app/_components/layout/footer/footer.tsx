5036bafdc189feb3a698cd7a538c9ab46c91238e
M-Mikos <mikos.marcin.m@gmail.com>
2025-04-17 13:15:36 +0200

    Add socials and links to footer

--- src/app/_components/layout/footer/footer.tsx
+++ src/app/_components/layout/footer/footer.tsx
@@ -1,9 +1,27 @@
+import Link from "next/link";
+import Logo from "../logo";
+import Navigation from "./navigation";
+import Socials from "./socials";
+import ContentContainer from "@/common/components/containers/content-container";
+
 function Footer() {
   return (
-    <footer className="flex flex-col gap-6">
-      <div></div>
-      <hr className="w-full border-t border-base-200" />
-      <div></div>
+    <footer>
+      <ContentContainer className="flex flex-col gap-6 py-32">
+        <div className="flex flex-col gap-3 sm:flex-row justify-between">
+          <div className="w-32 h-fit">
+            <Link title="Go to homepage" href="/">
+              <Logo />
+            </Link>
+          </div>
+          <Navigation />
+        </div>
+        <hr className="w-full border-t border-base-200" />
+        <div className="flex items-center flex-col gap-6 sm:flex-row sm:justify-between">
+          <span className="text-base-1000 opacity-50">©2021 Estatery. All rights reserved</span>
+          <Socials />
+        </div>
+      </ContentContainer>
     </footer>
   );
 }
