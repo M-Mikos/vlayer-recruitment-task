685f95d2372fe88c06cc956533b93a4224f117e7
M-Mikos <mikos.marcin.m@gmail.com>
2025-04-17 09:07:45 +0200

    Add header logo link and responsive nav structure

--- src/app/_components/layout/header/header.tsx
+++ src/app/_components/layout/header/header.tsx
@@ -1,9 +1,37 @@
+"use client";
+
+import ContentContainer from "@/common/components/containers/content-container";
 import Logo from "../logo";
+import Link from "next/link";
+import { useMediaQuery } from "@/common/hooks/use-media-query";
+import Actions from "./actions";
+import { Button } from "@/common/components/ui/button";
+import Navigation from "./navigation";
+import { useState } from "react";
 
 function Header() {
+  const isDesktop = useMediaQuery("(min-width:64em)");
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
   return (
     <header>
-      <Logo />
+      <ContentContainer>
+        <div className="w-32">
+          <Link title="Go to homepage" href="/">
+            <Logo />
+          </Link>
+        </div>
+        {isDesktop ? (
+          <div className="flex items-center gap-12">
+            <Navigation />
+            <Actions />
+          </div>
+        ) : (
+          <div className="flex items-center gap-4">
+            <Button onClick={() => setIsDrawerOpen(true)} variant="icon" className="px-2"></Button>
+          </div>
+        )}
+      </ContentContainer>
     </header>
   );
 }
